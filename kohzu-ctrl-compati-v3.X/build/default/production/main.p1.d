355c7d650560bb6d71188da51999b34c945b57fd
DiNOV-Tokyo <dddd3ddd@gmail.com>
2023-12-03 11:39:01 +0900

    feat: v3

---
+++ kohzu-ctrl-compati-v3.X/build/default/production/main.p1.d
@@ -0,0 +1,7 @@
+ build/default/production/main.d  \
+ build/default/production/main.p1:  \
+ main.c  \
+C:/Program\ Files/Microchip/MPLABX/v6.15/packs/Microchip/PIC12-16F1xxx_DFP/1.6.241/xc8/pic/include/pic.h  \
+uart.h  \
+C:/Program\ Files/Microchip/MPLABX/v6.15/packs/Microchip/PIC12-16F1xxx_DFP/1.6.241/xc8/pic/include/pic_chip_select.h  \
+C:/Program\ Files/Microchip/MPLABX/v6.15/packs/Microchip/PIC12-16F1xxx_DFP/1.6.241/xc8/pic/include/proc/pic16f1788.h 
